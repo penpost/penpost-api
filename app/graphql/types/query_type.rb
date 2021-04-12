3b5954842d4a5ba50f2013d2fc88beab34676c77
Yesi Meza <yesi.meza10@gmail.com>
2021-04-12 11:55:25 -0600

    Added query to find user by id
    Co-authored-by: Adam Etzion <60630167+aetzion1@users.noreply.github.com>

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -8,10 +8,17 @@ module Types
     # They will be entry points for queries on your schema.
 
     # TODO: remove me
-    field :test_field, String, null: false,
-      description: "An example field added by the generator"
-    def test_field
-      "Hello World!"
+    # field :test_field, String, null: false,
+    #   description: "An example field added by the generator"
+    # def test_field
+    #   "Hello World!"
+    # end
+
+    field :user, Types::UserType, null: false do 
+      argument :id, ID, required: true 
+    end
+    def user(id:)
+      User.find(id)
     end
   end
 end
