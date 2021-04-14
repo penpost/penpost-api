1b2255b646f0851eace503e3233c1cc483dcade4
Adam Etzion <60630167+aetzion1@users.noreply.github.com>
2021-04-14 17:21:29 -0600

    Update friendship_type.rb

--- app/graphql/types/friendship_type.rb
+++ app/graphql/types/friendship_type.rb
@@ -3,4 +3,4 @@ class Types::FriendshipType < Types::BaseObject
   field :user_1_id, Integer, null: false 
   field :user_2_id, Integer, null: false 
   field :current, Boolean, null: true 
-endp
+end
