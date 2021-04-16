a5031758a701324df31a55c42d3564fcb6c8c6c8
Yesi Meza <yesi.meza10@gmail.com>
2021-04-16 14:51:53 -0600

    Added error message test

--- spec/requests/mutations/random_pal_spec.rb
+++ spec/requests/mutations/random_pal_spec.rb
@@ -18,9 +18,7 @@ it "can create a random pen pal relationship" do
                         zip
                         country
                         }
-                    
-                    }
-                  "
+                    }"
 
   post graphql_path, params: { query: query_params }  
 
@@ -49,5 +47,40 @@ it "can create a random pen pal relationship" do
   expect(result[:data][:randomPal][:zip]).to be_a(String)
   expect(result[:data][:randomPal]).to have_key(:country)
   expect(result[:data][:randomPal][:country]).to be_a(String)
+end 
+it "will return error message if all users have penpals already" do
+  celine = User.create!(name: 'Celine Dion', email: 'titanic@example.com', password: 'pw1234', street: 'titanic ave', city: 'new york city', state: 'ny', zip: '55523', country: 'united states', address_verified: 1)
+  korra = User.create!(name: 'Korra Avatar', email: 'korra@example.com', password: 'pw1234', street: 'water bender st', city: 'republic city', state: 'ny', zip: '55123', country: 'united states', address_verified: 1)
+  connection1 = Friendship.create!(sender: celine, receiver: korra, current: true)
+
+
+  query_params =  "mutation {
+                    randomPal( input: {id: #{celine.id} } )
+                        {
+                        name
+                        email
+                        street
+                        city
+                        state
+                        zip
+                        country
+                        }
+                    }"
+
+  post graphql_path, params: { query: query_params }  
+
+  result = JSON.parse(response.body, symbolize_names: true )
+
+  expect(response).to be_successful 
+  expect(response.status).to eq(200)
+  expect(response.content_type).to eq("application/json; charset=utf-8")
+
+  expect(result).to have_key(:data)                          
+  expect(result[:data]).to be_a(Hash)                                                   
+  expect(result[:data]).to have_key(:randomPal)                         
+  expect(result[:errors]).to be_an(Array) 
+  expect(result[:errors][0]).to have_key(:message)                          
+  expect(result[:errors][0][:message]).to be_a(String) 
+
   end 
 end 
