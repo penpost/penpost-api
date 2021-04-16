df58ce300d035138d51a1a5a42ac2f669539ecd8
Adam Etzion <adametzion90@gmail.com>
2021-04-16 11:53:39 -0600

    Add graphql mutation to UpdateUser

--- app/graphql/mutations/update_user.rb
+++ app/graphql/mutations/update_user.rb
@@ -1,6 +1,10 @@
 class Mutations::UpdateUser < Mutations::BaseMutation
+  # class AuthProviderSignupData < Types::BaseInputObject
+  #   argument :credentials, Types::AuthProviderCredentialsInput, required: false
+  # end
+  
   argument :id, ID, required: true 
-  argument :password, String, required: false 
+  # argument :password, String, required: false 
   argument :name, String, required: false 
   argument :email, String, required: false 
   argument :description, String, required: false
@@ -10,21 +14,20 @@ class Mutations::UpdateUser < Mutations::BaseMutation
   argument :zip, String, required: false 
   argument :country, String, required: false 
   argument :address_verified, Boolean, required: false 
+  # argument :auth_provider, AuthProviderSignupData, required: false
 
   type Types::UserType
 
-  def resolve(id:, **attributes)
-    user = User.find(id)
+  def resolve(id:, **args)
     require 'pry'; binding.pry
-    user.update!(attributes)
-
+    user = User.find(id)
+    user.update!(args)
+    user
     # if (args.name !== nil)
     #   user.update(name: args[:name])
     # end
-
-    # if (args.description !== nil)
-    #   user.update(description: args[:description])
+    # if (args[:description] != nil)
+    #   user.update!(description: args[:description])
     # end
-
   end
 end
