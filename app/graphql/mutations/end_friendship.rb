f4b8ceaf474655813e3ac33309cc387adffc7962
Yesi Meza <yesi.meza10@gmail.com>
2021-04-19 13:59:22 -0600

    updated the end-friendshp mutation - allows receiver to end friendship
    Co-authored-by: Adam Etzion <60630167+aetzion1@users.noreply.github.com>

--- app/graphql/mutations/end_friendship.rb
+++ app/graphql/mutations/end_friendship.rb
@@ -3,13 +3,18 @@ class Mutations::EndFriendship < Mutations::BaseMutation
 
   type Types::FriendshipType
 
-
-  def resolve(args)
+   def resolve(args)
     user1 = User.find(args[:id]) 
-    friendship = user1.friendships_requested.where(current: true).first
+
+    if user1.friendships_requested.where(current: true) != [] 
+      friendship = user1.friendships_requested.where(current: true).first
+
+    elsif  user1.friendships_received.where(current: true) != [] 
+      friendship = user1.friendships_received.where(current: true).first
+    end
+
     friendship.update(current: false)
     friendship
-
   end
 
 
