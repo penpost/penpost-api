831abeb2025815d55fc97947480a5c305b2f8dff
Adam Etzion <adametzion90@gmail.com>
2021-04-19 08:38:27 -0600

    Update create user test, update mutationtype class

--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -1,5 +1,5 @@
 module Types
-  class MutationType < Types::BaseObject
+  class MutationType < BaseObject
  
     field :create_user, mutation: Mutations::CreateUser
     field :update_user, mutation: Mutations::UpdateUser
--- spec/requests/mutations/create_user_spec.rb
+++ spec/requests/mutations/create_user_spec.rb
@@ -1,30 +1,77 @@
 require 'rails_helper'
 
-RSpec.describe "Graphql create user mutation" do 
+RSpec.describe Mutations::CreateUser do
 
-  it 'can create new user' do
-    def perform(args = {})
-      Mutations::CreateUser.new(object: nil, field: nil, context: {}).resolve(args)
+  let(:migration) { Mutations::CreateUser.new(object: nil, field: nil, context: {}) }
+
+  describe 'CreateUser creates a User' do
+    it 'CREATES A USER' do
+          
+      #     user = perform(
+      #       name: 'Test User',
+      #       street: 'mulberry lane',
+      #       description: 'mememe',
+      #       city: 'denver',
+      #       state: 'co',
+      #       zip: '80218',
+      #       country: 'united states' ,    
+      #       auth_provider: {
+      #         credentials: {
+      #           email: 'email@example.com',
+      #           password: '[omitted]'
+      #         }
+      #       }
+      #     )
+
+      result = migration.resolve(
+          name: 'Test User',
+          street: 'mulberry lane',
+          description: 'mememe',
+          city: 'denver',
+          state: 'co',
+          zip: '80218',
+          country: 'united states' ,    
+          auth_provider: {
+            credentials: {
+              email: 'email@example.com',
+              password: '[omitted]'
+            }
+          }
+        )
+    # assert_equal user.name, 'Test User'
+    # assert_equal user.email, 'email@example.com'
+
+    expect(result[:name]).to eq('Test User')
+    expect(result[:errors]).to be_nil
     end
-  
-    user = perform(
-      name: 'Test User',
-      street: 'mulberry lane',
-      description: 'mememe',
-      city: 'denver',
-      state: 'co',
-      zip: '80218',
-      country: 'united states' ,    
-      auth_provider: {
-        credentials: {
-          email: 'email@example.com',
-          password: '[omitted]'
-        }
-      }
-    )
-
-    assert user.persisted?
-    assert_equal user.name, 'Test User'
-    assert_equal user.email, 'email@example.com'
   end
 end
+
+# RSpec.describe "Graphql create user mutation" do 
+#   it 'can create a user' do
+#     def perform(args = {})
+#       require 'pry'; binding.pry
+#       Mutations::CreateUser.new(object: nil, field: nil, context: {}).resolve(args)
+#     end
+  
+#     user = perform(
+#       name: 'Test User',
+#       street: 'mulberry lane',
+#       description: 'mememe',
+#       city: 'denver',
+#       state: 'co',
+#       zip: '80218',
+#       country: 'united states' ,    
+#       auth_provider: {
+#         credentials: {
+#           email: 'email@example.com',
+#           password: '[omitted]'
+#         }
+#       }
+#     )
+
+#     assert user.persisted?
+#     assert_equal user.name, 'Test User'
+#     assert_equal user.email, 'email@example.com'
+#   end
+# end
